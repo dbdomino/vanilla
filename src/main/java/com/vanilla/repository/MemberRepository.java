610bf19e9c7ec2ca4a6df67f33d99f9f6b2d33a6
minod <minod@naver.com>
2024-08-20 00:01:41 +0900

    repo

--- src/main/java/com/vanilla/repository/MemberRepository.java
+++ src/main/java/com/vanilla/repository/MemberRepository.java
@@ -9,11 +9,13 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * C `save()`
  * R `findOne()`
  * R `findByName()`
+ * R `findByIdPw()`
  * U `update()`
  * U `updateMemberPw()`
  * D `delete()`
@@ -33,10 +35,9 @@ public class MemberRepository {
     public Member findOne(String id) {
         TypedQuery<Member> query = em.createQuery("select m from Member m where m.memberId = :memberId and memberDel = 'N'",Member.class)
                 .setParameter("memberId", id);
-//        List<Member> members = query.getResultList(); // arrayList로 반환
-        Member member = query.getSingleResult(); // arrayList로 반환
+        Member member = query.getSingleResult();
+        // 단일 객체로 반환
         return member;
-//        return em.find(Member.class, id);
     }
     public List<Member> findByName(String memberName) {
         // named 쿼리
@@ -46,8 +47,16 @@ public class MemberRepository {
         // jpql 쿼리
         TypedQuery<Member> query = em.createQuery("select m from Member m where m.memberName = :memberName and memberDel='N' ",Member.class)
                 .setParameter("memberName", memberName);
-        List<Member> members = query.getResultList(); // arrayList로 반환
+        List<Member> members = query.getResultList();
+        // arrayList로 반환
         return members;
+    }
+    public Optional<Member> findByIdPw(String id, String pw) {
+        TypedQuery<Member> query = em.createQuery("select m from Member m where m.memberId = :memberId and memberPw = :memberPw and memberDel = 'N'",Member.class)
+                .setParameter("memberId", id)
+                .setParameter("memberPw", pw);
+        // Optional로 감싸서 반환
+        return query.getResultStream().findFirst();
     }
 
     // U
