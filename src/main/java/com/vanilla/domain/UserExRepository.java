3f75ec935eb88537c18d30bef582e5dc13e67596
minod <minod@naver.com>
2024-09-17 23:50:39 +0900

    userEx disable

--- src/main/java/com/vanilla/domain/UserExRepository.java
+++ src/main/java/com/vanilla/domain/UserExRepository.java
@@ -1,8 +1,8 @@
-package com.vanilla.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// data jpa
-public interface UserExRepository extends JpaRepository<UserEx, Long> {
-
-}
+//package com.vanilla.domain;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//// data jpa
+//public interface UserExRepository extends JpaRepository<UserEx, Long> {
+//
+//}
