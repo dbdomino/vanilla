856653a9ebab6e3f98699b87d6f55ffe8e45bfa9
minod <minod@naver.com>
2024-09-17 23:51:14 +0900

    Member Service

--- src/main/java/com/vanilla/service/MemberService.java
+++ src/main/java/com/vanilla/service/MemberService.java
@@ -13,9 +13,27 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    public Member memberSearch(String memberId, String memberPw) {
+        Optional<Member> memberOptional = memberRepository.findByIdPw(memberId, memberPw);
+        if (memberOptional.isPresent()){
+            return memberOptional.get();
+        } else {
+            throw new RuntimeException("E110");
+        }
+
+
+    }
+
     public boolean login(String memberId, String memberPw) {
         Optional<Member> memberOptional = memberRepository.findByIdPw(memberId, memberPw);
         return memberOptional.isPresent();
         // 로그인 성공이면 true, 실패면 false
+    }
+
+    public Member signUp(String memberId, String memberPw, String memberName, String memberEmail, String memberPhone, String memberAddress) {
+
+        memberRepository.save( memberId, memberPw,memberName,memberEmail,memberPhone,memberAddress);
+        // 로그인 성공이면 true, 실패면 false
+        return memberRepository.findOne(memberId);
     }
 }
