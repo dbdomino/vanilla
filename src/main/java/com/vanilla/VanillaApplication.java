a1bdddb88314c1c37885b6c66f73b449088728f5
minod <minod@naver.com>
2024-09-17 23:50:49 +0900

    main

--- src/main/java/com/vanilla/VanillaApplication.java
+++ src/main/java/com/vanilla/VanillaApplication.java
@@ -3,11 +3,12 @@ package com.vanilla;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.vanilla")
 public class VanillaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VanillaApplication.class, args);
+
 	}
 
 }
