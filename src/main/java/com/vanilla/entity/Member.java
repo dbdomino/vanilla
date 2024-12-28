0d0ab244c1c1c2187646f958bdfac80088ed9011
jhbae <jhbae@telcoware.com>
2024-12-28 21:16:05 +0900

    [Entity] Member

--- src/main/java/com/vanilla/entity/Member.java
+++ src/main/java/com/vanilla/entity/Member.java
@@ -1,40 +1,68 @@
 package com.vanilla.entity;
 
 import com.vanilla.entity.superclass.CuDate;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.Comment;
 
 @Entity
 @Getter @Setter @ToString
 public class Member extends CuDate {
-    @Id
-    private String memberId;
-    private String memberPw;
+    @Id @Column(nullable = false, unique = true)
+    @Comment("회원 id")
+    private String id;
 
-    private String memberName;
-    private String memberEmail;
-    private String memberPhone;
-    private String memberAddress;
+    @Comment("회원 PW")
+    @NotEmpty
+    private String pw;
 
-    private String memberType= "N3";
-    private String memberDel = "N";
+    @Comment("회원 이름")
+    private String name;
+    @Comment("회원 이메일")
+    private String email;
+    @Comment("회원 휴대폰번호")
+    private String phone;
+    @Comment("회원 주소")
+    private String address;
+
+    @Comment("회원 유형 0: 일반 사용자  1: 관리자")
+    private String type = "0";
+    @Comment("회원 삭제유무")
+    private String isDel = "N";
 
 
-//    public List<PaymentMethod> getPaymentMethods() {
-//
-//        return paymentMethods;
-//    }
+    // Default constructor (required by JPA)
+    public Member() {
+    }
+
+    // Constructor with fields
+    public Member(String id, String pw) {
+        this.id = id;
+        this.pw = name;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "ExampleEntity{" +
+                "id='" + id + '\'' +
+                ", pw='" + pw + '\'' +
+                '}';
+    }
 
     public void setMember(String memberId, String memberPw, String memberName, String memberEmail,String memberPhone,String memberAddress ){
-        this.memberId = memberId;
-        this.memberPw = memberPw;
-        this.memberName = memberName;
-        this.memberEmail = memberEmail;
-        this.memberPhone = memberPhone;
-        this.memberAddress = memberAddress;
+        this.id = memberId;
+        this.pw = memberPw;
+        this.name = memberName;
+        this.email = memberEmail;
+        this.phone = memberPhone;
+        this.address = memberAddress;
 
     }
 
