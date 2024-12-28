27af16d36f1fae35cdd8a41058916ac8de19187f
jhbae <jhbae@telcoware.com>
2024-12-28 21:17:24 +0900

    [repository] Ammount inf

---
+++ src/main/java/com/vanilla/repository/itf/AmmountRepository.java
@@ -0,0 +1,9 @@
+package com.vanilla.repository.itf;
+
+import com.vanilla.entity.Ammount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface AmmountRepository extends JpaRepository<Ammount, Integer> {
+
+}
