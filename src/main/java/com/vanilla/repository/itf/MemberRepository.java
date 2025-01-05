9476002dfd664596101aecee3a6656bed81d38a9
jhbae <jhbae@telcoware.com>
2025-01-05 19:37:39 +0900

    Member Repo

---
+++ src/main/java/com/vanilla/repository/itf/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.vanilla.repository.itf;
+
+import com.vanilla.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**  @since  2024-12-28
+ * @author dbdomino
+ * @version 1.0
+ * Member
+ */
+//public interface MemberRepository extends JpaRepository<Member, Integer> {
+public interface MemberRepository {
+
+}
