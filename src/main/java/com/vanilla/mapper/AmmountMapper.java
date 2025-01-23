7337a226a78d7f74312a33d653a7aec5e3690420
jhbae <jhbae@telcoware.com>
2025-01-23 17:19:37 +0900

    Ammount 갱신

--- src/main/java/com/vanilla/mapper/AmmountMapper.java
+++ src/main/java/com/vanilla/mapper/AmmountMapper.java
@@ -1,9 +1,13 @@
 package com.vanilla.mapper;
 
+import com.vanilla.dto.AmmountDto;
 import com.vanilla.dto.response.AmmountResponseDto;
 import com.vanilla.dto.service.AmmountServiceDto;
 import com.vanilla.entity.Ammount;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 import java.util.List;
 /** @since  2024-12-28
@@ -14,11 +18,17 @@ import java.util.List;
 @Mapper(componentModel = "spring")
 public interface AmmountMapper {
 //    AmmountMapper INSTANCE = Mappers.getMapper(AmmountMapper.class);
+
     // 매핑 메서드 예시
-    AmmountServiceDto toServiceDto(Ammount entity);
-    AmmountResponseDto toResponseDto(AmmountServiceDto entity);
+    AmmountServiceDto toServiceDto(Ammount dto);
+    AmmountResponseDto toResponseDto(AmmountServiceDto dto);
+    AmmountDto toDto(Ammount entity);
     Ammount toEntity(AmmountServiceDto dto);
     List<AmmountServiceDto> toServiceDtoList(List<Ammount> ammountList);
     List<AmmountResponseDto> toResponseDtoList(List<AmmountServiceDto> serviceDtoList);
     // dto와 dto간의 매핑도 가능,
+
+    // DTO에 값 중에 null이 아닌 값만 Entity로 Mapping시킴
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Ammount partialUpdate(AmmountDto dto, @MappingTarget Ammount entity);
 }
